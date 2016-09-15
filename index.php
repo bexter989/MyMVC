dbe9948bc1477e9f32d8a0169d6f6710f84319f3
Bexter <solienbernard989@gmail.com>
2016-09-15 02:42:47 -0700

    Clean up

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 ini_set('display_errors', 1);
 
 // Base Url
-define('base_url', 'http://localhost/MyMVC/');
+define('base_url', 'http://localhost/');
 
 /*Require the Bootstrap file */
 require 'libs/Core.php';
