edfc652484b7b62c523d8c6778ba787f125b42e9
Bernard Solien Win-10-WSL <solienbernard989@gmail.com>
2020-10-29 19:37:36 +1000

    Updates

--- application/views/header.php
+++ application/views/header.php
@@ -5,6 +5,6 @@
             <?php echo (isset($title) ? $title : "Welcome"); ?>
         </title>
         <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-        <!-- <link rel="stylesheet" href="/css/bootstrap.min.css" /> -->
+        <link rel="stylesheet" href="/css/bootstrap.min.css" />
     </head>
     <body>
